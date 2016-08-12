558be51c7c4c2bd868062a7eb505f751340115db
n2omatt <n2o.matt@gmail.com>
2016-08-12 16:46:09 -0300

    - Update the license header. - Change the license from BSD-3 to GPLv3. - Implement the class.

--- include/Localization/StringsManager.h
+++ include/Localization/StringsManager.h
@@ -1,18 +1,17 @@
-//----------------------------------------------------------------------------//
-//                 █      █                                                   //
-//                 ████████                                                   //
-//               ██        ██                                                 //
-//              ███  █  █  ███    StringsManager.h                            //
-//              █ █        █ █    MonsterFramework                            //
-//               ████████████                                                 //
-//             █              █   Copyright (c) 2015 AmazingCow               //
-//            █     █    █     █  www.AmazingCow.com                          //
-//            █     █    █     █                                              //
-//             █              █   N2OMatt - n2omatt@amazingcow.com            //
-//               ████████████     www.amazingcow.com/n2omatt                  //
+﻿//----------------------------------------------------------------------------//
+//               █      █                                                     //
+//               ████████                                                     //
+//             ██        ██                                                   //
+//            ███  █  █  ███        ILoadResolver.h                           //
+//            █ █        █ █        MonsterFramework                          //
+//             ████████████                                                   //
+//           █              █       Copyright (c) 2015, 2016                  //
+//          █     █    █     █      AmazingCow - www.AmazingCow.com           //
+//          █     █    █     █                                                //
+//           █              █       N2OMatt - n2omatt@amazingcow.com          //
+//             ████████████         www.amazingcow.com/n2omatt                //
 //                                                                            //
-//                                                                            //
-//                  This software is licensed as BSD-3                        //
+//                  This software is licensed as GPLv3                        //
 //                 CHECK THE COPYING FILE TO MORE DETAILS                     //
 //                                                                            //
 //    Permission is granted to anyone to use this software for any purpose,   //
@@ -27,9 +26,9 @@
 //        (See opensource.AmazingCow.com/acknowledgment.html for details).    //
 //        If you will not acknowledge, just send us a email. We'll be         //
 //        *VERY* happy to see our work being used by other people. :)         //
-//        The email is: acknowledgmentopensource@AmazingCow.com               //
+//        The email is: acknowledgment_opensource@AmazingCow.com              //
 //     3. Altered source versions must be plainly marked as such,             //
-//        and must notbe misrepresented as being the original software.       //
+//        and must not be misrepresented as being the original software.      //
 //     4. This notice may not be removed or altered from any source           //
 //        distribution.                                                       //
 //     5. Most important, you must have fun. ;)                               //
@@ -43,35 +42,66 @@
 #define __MonsterFramework_include_Localization_StringsManager_h__
 
 //std
+#include <cstdarg>
 #include <string>
+#include <unordered_map>
+
 //MonsterFramework
 #include "MonsterFramework/include/Utils/MonsterFramework_Utils.h"
 
 NS_MF_BEGIN
 class StringsManager
 {
+    // Enums / Constants / Typedefs //
+public:
+    typedef std::unordered_map<std::string, std::string> StringsMap;
+    typedef std::unordered_map<std::string, StringsMap>  LanguageMap;
+
+
     // Singleton //
 public:
     SINGLETON_OF(StringsManager);
 
+
     // CTOR/DTOR //
 private:
-    StringsManager()  { /* Empty... */ }
+    StringsManager () { /* Empty... */ }
     ~StringsManager() { /* Empty... */ }
+
 
     // Public Methods //
 public:
-    void loadStringsFile(const std::string &filename);
-    const std::string getString(const std::string &path);
+    //Load / Purge
+    void loadStrings(const std::string &filename,
+                     const std::string &languageName);
 
-	// Private Methods //
+    void purgeStrings(const std::string &languageName);
+
+
+    //Get String
+    std::string getString(const char *string, ...) const;
+
+    //Language Map
+    const std::string& getCurrentLanguageName() const;
+    void setCurrentLanguageName(const std::string &languageName);
+
+
+    // Private Methods //
 private:
-	std::string getLocalizedFilename(const std::string &filename);
+    void parseStringsFile(const std::string &fullname,
+                          const std::string &languageName);
+
 
     // iVars //
 private:
+    LanguageMap m_languageMap;
 
+    std::string m_currLanguageName;
+    StringsMap *m_pCurrStringsMap;
 };
+
+#define MF_LocalizedString(_str_, ...) \
+    StringsManager::instance()->getString(_str_, ##__VA_ARGS__)
 
 NS_MF_END
 #endif // defined(__MonsterFramework_include_Localization_StringsManager_h__) //
