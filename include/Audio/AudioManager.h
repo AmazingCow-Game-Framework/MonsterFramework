7b43e16ca2c77d0f00b1b87865d32c277bdaf99b
n2omatt <n2o.matt@gmail.com>
2016-08-12 16:34:02 -0300

    - Update the license header. - Change the license type from BSD-3 to GPLv3. - New API.

--- include/Audio/AudioManager.h
+++ include/Audio/AudioManager.h
@@ -1,18 +1,17 @@
-//----------------------------------------------------------------------------//
-//                 █      █                                                   //
-//                 ████████                                                   //
-//               ██        ██                                                 //
-//              ███  █  █  ███    AudioManager.h                              //
-//              █ █        █ █    MonsterFramework                            //
-//               ████████████                                                 //
-//             █              █   Copyright (c) 2015 AmazingCow               //
-//            █     █    █     █  www.AmazingCow.com                          //
-//            █     █    █     █                                              //
-//             █              █   N2OMatt - n2omatt@amazingcow.com            //
-//               ████████████     www.amazingcow.com/n2omatt                  //
+﻿//----------------------------------------------------------------------------//
+//               █      █                                                     //
+//               ████████                                                     //
+//             ██        ██                                                   //
+//            ███  █  █  ███        AudioManager.h                            //
+//            █ █        █ █        MonsterFramework                          //
+//             ████████████                                                   //
+//           █              █       Copyright (c) 2015, 2016                  //
+//          █     █    █     █      AmazingCow - www.AmazingCow.com           //
+//          █     █    █     █                                                //
+//           █              █       N2OMatt - n2omatt@amazingcow.com          //
+//             ████████████         www.amazingcow.com/n2omatt                //
 //                                                                            //
-//                                                                            //
-//                  This software is licensed as BSD-3                        //
+//                  This software is licensed as GPLv3                        //
 //                 CHECK THE COPYING FILE TO MORE DETAILS                     //
 //                                                                            //
 //    Permission is granted to anyone to use this software for any purpose,   //
@@ -27,9 +26,9 @@
 //        (See opensource.AmazingCow.com/acknowledgment.html for details).    //
 //        If you will not acknowledge, just send us a email. We'll be         //
 //        *VERY* happy to see our work being used by other people. :)         //
-//        The email is: acknowledgmentopensource@AmazingCow.com               //
+//        The email is: acknowledgment_opensource@AmazingCow.com              //
 //     3. Altered source versions must be plainly marked as such,             //
-//        and must notbe misrepresented as being the original software.       //
+//        and must not be misrepresented as being the original software.      //
 //     4. This notice may not be removed or altered from any source           //
 //        distribution.                                                       //
 //     5. Most important, you must have fun. ;)                               //
@@ -50,54 +49,101 @@
 //MonsterFramework
 #include "MonsterFramework/include/Utils/MonsterFramework_Utils.h"
 
+
 NS_MF_BEGIN
 
 class AudioManager
 {
-    // Enums/Constants/Typedefs //
+    // Inner Types //
 private:
-    typedef std::map<std::string, std::pair<std::string, int>> SoundsMap;
-    
+    struct Sound {
+        int currInternalId;
+        std::string filepath;
+    };
+
+
+    // Enums / Constants / Typedefs //
+public:
+    typedef std::function<void (const std::string &)> EffectFinishCallback;
+    typedef std::function<void (const std::string &)> MusicFinishCallback;
+
+    const static int kInvalidId   = -123456789;
+    const static int kInvalidTime = -987654321;
+
+private:
+    typedef std::map<std::string, Sound> SoundMap;
+
+
     // Singleton //
 public:
-    static AudioManager* instance();
-
-    // CTOR/DTOR //
+    SINGLETON_OF(AudioManager);
 private:
-    AudioManager();
-    virtual ~AudioManager();
+     AudioManager();
+    ~AudioManager();
 
-    // Public Methods //
+
+    // General //
 public:
-    //General
     void muteAll(bool b);
     bool isMuted();
 
-    //Effects
-    void loadEffect(const std::string &effectId, const std::string &path);
+
+    // Effects //
+private:
+    //Load / Unload
+    void loadEffect  (const std::string &effectId, const std::string &path);
     void unloadEffect(const std::string &effectId);
 
+    //Play / Pause
     void playEffect(const std::string &effectId, bool loop = false);
 
     void pauseEffect(const std::string &effectId);
     void pauseAllEffects();
 
+    //Resume
     void resumeEffect(const std::string &effectId);
     void resumeAllEffects();
 
+    //Stop
     void stopEffect(const std::string &effectId);
     void stopAllEffects();
 
-    float getEffectsVolume();
-    void setEffectsVolume(float volume);
+    //Query
+    float getEffectsVolume() const;
+    void  setEffectsVolume(float volume);
+
+
+    // Music //
+public:
+    //Load / Unload
+    void loadMusic  (const std::string &musicId, const std::string &path);
+    void unloadMusic(const std::string &musicId);
+
+    //Play / Pause / Resume / Stop
+    void playMusic(const std::string &musicId,
+                   float volume = 1.0f,
+                   bool  loop   = false,
+                   const MusicFinishCallback &callback = MusicFinishCallback());
+
+    void pauseMusic (const std::string &musicId);
+    void resumeMusic(const std::string &musicId);
+    void stopMusic  (const std::string &musicId, bool unload = false);
+
+    //Music Time
+    float getMusicTotalTime  (const std::string &musicId);
+    float getMusicCurrentTime(const std::string &musicId);
+    void  setMusicCurrentTime(const std::string &musicId, float time);
+
+    //Query
+    bool isPlayingMusic(const std::string &musicId);
+
+    float getMusicVolume() const;
+    void  setMusicVolume(float volume);
 
     // iVars //
 private:
-    cd::SimpleAudioEngine *m_pAudioEngine;
-    SoundsMap              m_effectsMap;
+    SoundMap m_musicMap;
 
-    float m_prevFxVol;
-    
 }; //class AudioManager
 
 NS_MF_END
