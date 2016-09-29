8e35aecc9319a8e346337169b2c0961832aae452
n2omatt <n2o.matt@gmail.com>
2016-09-28 21:21:53 -0300

    - Add utility method.

--- include/Graphics/GraphicsHelper.h
+++ include/Graphics/GraphicsHelper.h
@@ -61,7 +61,8 @@ public:
     static void copyPosition   (cc::Node *pSrc, cc::Node *pDst);
     static void copySize       (cc::Node *pSrc, cc::Node *pDst);
     static void copyAnchorPoint(cc::Node *pSrc, cc::Node *pDst);
-
+    static void copyScale      (cc::Node *pSrc, cc::Node *pDst);
+    
     // ??? //
 public:
     static cc::Vec2 getSizeScaleRatio(cc::Node *pNode1, cc::Node *pNode2);
